eef9ecad3915b2c36a61020b19ab4ed0f5881b47
kelvin <calvinisaacphiri@gmail.com>
2025-03-29 12:17:45 +0200

    also log errors of diff sort

--- src/LoggerServiceProvider.php
+++ src/LoggerServiceProvider.php
@@ -5,8 +5,8 @@ namespace CavinKim\LaravelTerminalLogger;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\Route;
 use Illuminate\View\Events\ViewRendered;
+use Illuminate\Log\Events\MessageLogged;
 
 class LoggerServiceProvider extends ServiceProvider
 {
@@ -26,10 +26,31 @@ class LoggerServiceProvider extends ServiceProvider
         Event::listen(ViewRendered::class, function ($event) {
             Logger::view($event->view->getName(), $event->view->getData());
         });
+
+        // Log Laravel errors (exceptions and logged messages)
+        Event::listen(MessageLogged::class, function ($event) {
+            if (isset($event->context['exception'])) {
+                // Log exceptions with detailed information
+                $exception = $event->context['exception'];
+                Logger::log(
+                    'ERROR',
+                    sprintf(
+                        '%s [%s %s] in %s:%d',
+                        $exception->getMessage(),
+                        request()->method(),
+                        request()->fullUrl(),
+                        $exception->getFile(),
+                        $exception->getLine()
+                    )
+                );
+            } else {
+                // Log other logged messages
+                Logger::log('ERROR', $event->message);
+            }
+        });
     }
 
     public function register()
-    {
-     
+    {        
     }
 }
